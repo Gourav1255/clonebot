1d2f6d9fb1d48e7ac45cbdf248c954c8bf45137c
Gourav1255 <114558061+Gourav1255@users.noreply.github.com>
2022-12-30 10:34:50 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -22,7 +22,7 @@ class Config(object):
 7ee46e0888432fad23173820d4caddf2")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "1BVtsOLsBu8My1rx6M8tpfWOVdH3R2DbDh12E11T3q64oY8p9T3J0erY9i0tL-YFzOtPOVnwVebeYxy--wN8B2OIVMwISmhtgtTZ03NwNFPjwdExVp1n9EgqXaUGmTs9wH3CoqW_-_oB405svEfCXv-E4wNYRxsox1CM9i74a9J4RFSaFmdnG9e6CfV8PC-B-syAgbQMT9MftwBe4k5l1F4I3niwYA6El8Vw9rxuZYVrJDkHWgB8VLcQYYhoC1eNZE2ruA-Nf9XlplO47HFINFoBy2xNVkJsgU3MpVNwJJJnRq5hvK5Z-qYwm-Go0ve3wzMFEPG6o2HLSI1u_w0qv2wDPvtZK3YQ=")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "1BVtsOIUBuxFyf670u4hfE_n69_oRqZMd2HQm-BT_CHYEJLh9r4d4AG6U-yN1c3dPCkySsZ55Tlicyo74ukoYMRp8EjxesjfJO0I0OsliS5P26B5w5zr13MphGdvYzUvhGLEMueue1oewYQlFsAJ5kbiqUa2O1QBw5-h11N8Uwq9AmBnmiMAvV-YPkQ9fpq429B3rA22iQftKU8FnU3kICzt6gDrXXW8EqlklViD3ejQZflDMslyEf_5BgZ7sUpa3fB8yG7T4l50PD6hskpH_yhwmiQj5XaS-v-5qnqrr3nxoRCl2I9Qv5QkgkcUVV2b6VxEidT8m2OfX1eCX-l3vJVMyiML15CQ=")
 
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://clonetgbot:clonetgbot@cluster0.v8hwx1m.mongodb.net/?retryWrites=true&w=majority")
